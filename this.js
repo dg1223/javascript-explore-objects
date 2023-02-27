a773bb16b2a21fe087be889af5b360903de364c1
dg1223 <alavi1223@hotmail.com>
2023-02-27 01:18:08 -0500

    this

---
+++ this.js
@@ -0,0 +1,48 @@
+const kodomAli = {
+  name: "Kodom Ali",
+  money: 5000,
+  study: "Math",
+  subjects: ["calculus", "algebra", "geometry"],
+  exam: function () {
+    // console.log(this.name, "is participating in an exam");
+    // return this.name + " is participating in an exam";
+    console.log(this);
+  },
+  examArrow: () => {
+    console.log(this);
+  },
+  examNested: () => {
+    const arrow = () => {
+      console.log(this);
+    };
+    arrow();
+  },
+  improveExam: function (subject) {
+    this.exam();
+    return `${this.name} is taking improvement exam on ${subject}`;
+  },
+  treatDey: function (amount, tips) {
+    this.money = this.money - amount - tips;
+    return this.money;
+  },
+};
+
+kodomAli.exam();
+// kodomAli.examArrow();
+// kodomAli.examNested();
+
+const badamAli = {
+  name: "kacha badam",
+  money: 8000,
+};
+
+badamAli.exam = kodomAli.exam;
+// badamAli.exam();
+
+function clickHandler() {
+  console.log("inside click handler", this);
+}
+
+document.getElementById("btn-click2").addEventListener("click", function () {
+  console.log(this);
+});
