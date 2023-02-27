4b896e1699884bec1420ba4bb498a92965ff61a8
dg1223 <alavi1223@hotmail.com>
2023-02-27 00:30:15 -0500

    object comparison

---
+++ compare.js
@@ -0,0 +1,40 @@
+// const first = {};
+// const second = {};
+// const third = second;
+// if (third === second) {
+// //   console.log("they are same");
+// } else {
+// //   console.log("they are different");
+// }
+
+const { coolGray } = require("tailwindcss/colors");
+
+// do not use this method to compare object or array
+const first = { a: 5, b: 2, d: 5 };
+const second = { a: 5, b: 2, c: 3 };
+const firstString = JSON.stringify(first);
+const secondString = JSON.stringify(second);
+// console.log(firstString, secondString);
+
+// if (firstString === secondString) {
+//   console.log("same");
+// } else {
+//   console.log("different");
+// }
+
+function compareObject(first, second) {
+  const firstKeys = Object.keys(first);
+  const secondKeys = Object.keys(second);
+  if (firstKeys.length === secondKeys.length) {
+    for (const key of firstKeys) {
+      if (first[key] !== second[key]) {
+        return true;
+      }
+    }
+  } else {
+    return false;
+  }
+}
+
+const isSame = compareObject(first, second);
+console.log(isSame);
